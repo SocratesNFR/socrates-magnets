0242d43861b434dcf49aeef80b841dab5c408ac3
Johannes H. Jensen <joh@pseudoberries.com>
2017-10-01 17:09:25 +0200

    Add rudimentary sweep run

---
+++ run_sweep.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import os
+import time
+import argparse
+from mx3util import gen_job, run_local, run_dist
+import numpy as np
+
+n_gpus_dist = 2
+
+def main(args):
+    mx = np.linspace(-1, 1, 11)
+
+    queue = []
+
+    for i, x in enumerate(mx):
+        base, ext = os.path.splitext(os.path.basename(args.template))
+        outfile = "{}.{:03d}{}".format(base, i, ext)
+        out = os.path.join(args.outdir, outfile)
+        params = {'mx': x, 'my': 1, 'mz': 0}
+        gen_job(args.template, out, **params)
+        queue.append(out)
+
+    if args.run == 'local':
+        while queue:
+            # TODO: We assume only one gpu
+            jobs = queue.pop(0)
+            run_local([jobs])
+
+    elif args.run == 'dist':
+        # Submit jobs in chunks of n_gpus_dist
+        procs = []
+        while queue:
+            jobs = queue[0:n_gpus_dist]
+            del queue[0:n_gpus_dist]
+            p = run_dist(jobs, wait=False)
+            procs.append(p)
+
+        # Wait for all jobs to finish
+        while not all([p.poll() is not None for p in procs]):
+            time.sleep(1)
+
+class StoreKeyValue(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        k, v = values.split('=')
+        if getattr(namespace, self.dest) is None:
+            setattr(namespace, self.dest, {})
+        d = getattr(namespace, self.dest)
+        d[k] = v
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run mx3 job')
+    parser.add_argument('-r', '--run', choices=['local', 'dist'], default='local',
+                        help='run locally or distributed on a cluster')
+    parser.add_argument('-p', '--param', action=StoreKeyValue,
+                        help='set template parameter key=value')
+    parser.add_argument('template', help='job template')
+    parser.add_argument('outdir', help='output directory for job files')
+
+    args = parser.parse_args()
+    main(args)
