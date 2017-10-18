6bd46869a8861d1007009708759bd55a82ddd259
Johannes H. Jensen <joh@pseudoberries.com>
2017-10-18 16:53:49 +0200

    Put digitized data in its own subplot

--- plot_table.py
+++ plot_table.py
@@ -36,25 +36,35 @@ def main(args):
 
     data = load_table(args.filename, variables)
 
+    n_rows = 1
+    if args.digitize:
+        n_rows += 1
+    fig, axes = plt.subplots(n_rows, 1, sharex=True, sharey=False)
+    axes = np.atleast_1d(axes)
+    axes = axes.flatten()
+
     t = data[:,0] # first is always time
     for i, v in enumerate(variables[1:], start=1):
         d = data[:,i]
-        plt.plot(t, d, label=v)
+        line, = axes[0].plot(t, d, label=v)
 
         if args.digitize:
             dd = np.where(d > 0, 1, 0)
-            plt.plot(t, -1.5 + 0.2 * dd)
+            axes[1].plot(t, dd - 1.1*(i - 1), color=line.get_color())
 
 
     # Shrink current axis by 10%
-    ax = plt.gca()
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.9, box.height])
+    for ax in axes:
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0, box.width * 0.9, box.height])
 
-    plt.title(args.filename)
-    plt.xlabel("t")
-    plt.legend(loc='upper left', bbox_to_anchor=(1.0, 1.0))
-    plt.xlim(0, t[-1])
+    fig.suptitle(args.filename)
+    axes[0].legend(loc='upper left', bbox_to_anchor=(1.0, 1.0))
+    axes[0].set_xlim(0, t[-1])
+
+    axes[-1].set_xlabel("t")
+    if args.digitize:
+        axes[1].get_yaxis().set_visible(False)
 
     if args.output:
         plt.savefig(args.output)
