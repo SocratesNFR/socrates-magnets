11da6df2c732050e4ceca4c6432af822db5978df
Johannes H. Jensen <joh@pseudoberries.com>
2017-12-05 08:57:42 +0100

    Add --interactive option

--- run.py
+++ run.py
@@ -4,17 +4,21 @@ from mx3util import gen_job, run_local, run_dist, StoreKeyValue
 
 def main(args):
     gen_job(args.template, args.out, **args.param)
+
     if args.run == 'local':
-        run_local([args.out])
+        run_local([args.out], interactive=args.interactive)
 
     elif args.run == 'dist':
+        assert not args.interactive
         run_dist([args.out])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run mx3 job')
     parser.add_argument('-r', '--run', choices=['local', 'dist'], default='local',
                         help='run locally or distributed on a cluster')
-    parser.add_argument('-p', '--param', action=StoreKeyValue,
+    parser.add_argument('-i', '--interactive', action='store_true',
+                        help='run mumax3 interactively')
+    parser.add_argument('-p', '--param', action=StoreKeyValue, default={},
                         help='set template parameter key=value')
     parser.add_argument('template', help='job template')
     parser.add_argument('out', help='output job file')
