a1406c77e836ed0d9f89f51a01843477ffa8c11d
Johannes H. Jensen <joh@pseudoberries.com>
2018-01-18 16:25:10 +0100

    Add support for more stats, support multi-run

--- states.py
+++ states.py
@@ -44,10 +44,29 @@ def load_data(tablefile, variables, spp=100, skip=1):
     return DX
 
 def unique_states(X):
-    return np.unique(X, axis=0)
+    # Concatenate runs
+    return np.unique(np.concatenate(X), axis=0)
 
 def count_states(X):
     return len(unique_states(X))
+
+def final_states(X):
+    return X[None, :, -1] # retain shape
+
+def count_final_states(X):
+    X = final_states(X)
+    return count_states(X)
+
+def count_diff(X):
+    # Treat runs separately
+    diff = np.count_nonzero(X[:,:-1] != X[:,1:], axis=2)
+    return np.sum(diff, axis=1)
+
+stats_available = {
+    'state_count': count_states,
+    'state_diff': count_diff,
+    'final_count': count_final_states,
+}
 
 def main(args):
     basedir = os.path.dirname(args.filename)
@@ -58,13 +77,15 @@ def main(args):
     sweep_spec = sweep_spec[0]
     sweep_param = sweep_spec[0][0]
     sweep_values = [sp[1] for sp in sweep_spec]
+    stat_fn = stats_available[args.stat]
 
     print("#Parameter values: {}".format(len(sweep_values)))
-    print("#Runs per value: {}".format(len(run_info[0])))
+    print("#Runs per value: {}".format(list(map(len, run_info))))
     print("Sweep parameter: {}".format(sweep_param))
     print("Parameter range: {}..{} [{}]".format(
         np.min(sweep_values), np.max(sweep_values),
         sweep_values[1] - sweep_values[0]))
+    print("Statistic: {}".format(args.stat))
 
     # Learn available variables from first run
     mx3_filename = os.path.join(basedir, run_info[0][0]['filename'])
@@ -76,30 +97,37 @@ def main(args):
 
     n_vars = len(variables)
 
+    stats = np.zeros(len(sweep_values), dtype=int)
     state_count = np.zeros(len(sweep_values), dtype=int)
 
     for j, runs in enumerate(run_info):
-        for run in runs:
-            # print(".", end='', flush=True)
-            mx3_filename = os.path.join(basedir, run['filename'])
-            tablefile = get_tablefile(mx3_filename)
-            X = load_data(tablefile, variables, args.spp, args.skip)
-            # states = unique_states(X)
-            state_count[j] = count_states(X)
-            # print(state_count[j], states)
-            print(state_count[j], end=' ', flush=True)
+        # assert len(runs) == 1, ">1 run not yet implemented"
+        X = []
+        try:
+            for run in runs:
+                mx3_filename = os.path.join(basedir, run['filename'])
+                tablefile = get_tablefile(mx3_filename)
+                Xi = load_data(tablefile, variables, args.spp, args.skip)
+                X.append(Xi)
+        except FileNotFoundError:
+            print("incomplete", end=' ', flush=True)
+            continue
+
+        X = np.array(X)
+        stats[j] = stat_fn(X)
+        print(stats[j], end=' ', flush=True)
 
     print("")
 
 
-    plt.plot(sweep_values, state_count, 'o-')
+    plt.plot(sweep_values, stats, 'o-')
 
     # TODO: Better title
     title = str(info['args'].outdir)
     plt.title(title)
 
     plt.xlabel(sweep_param)
-    plt.ylabel("#states")
+    plt.ylabel(args.stat)
 
     if args.savefig:
         filename = args.savefig
@@ -121,6 +149,8 @@ if __name__ == '__main__':
     parser.add_argument('filename', help='run_info file')
     parser.add_argument('var', nargs='+',
                         help='list of variables to plot')
+    parser.add_argument('-t', '--stat', choices=stats_available.keys(),
+            default='state_count')
     parser.add_argument('-s', '--spp', type=int, default=100,
             help='Samples per period')
     parser.add_argument('-k', '--skip', type=float, default=0,
