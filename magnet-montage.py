4b3e221dd9cb7df275a74b8102cb60c0775e16f1
Johannes H. Jensen <joh@pseudoberries.com>
2018-05-22 17:20:01 +0200

    Use jinja2 for montage labels

--- magnet-montage.py
+++ magnet-montage.py
@@ -2,6 +2,7 @@
 import os
 import subprocess
 import glob
+from jinja2 import Template
 
 def montage(output_file, input_files, labels=None):
     cmd = ['montage', '-background', 'black', '-fill', 'white']
@@ -15,19 +16,20 @@ def montage(output_file, input_files, labels=None):
 
     return subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-def gen_labels(input_files, fmt="{}"):
-    inds = range(len(input_files))
-    return map(fmt.format, inds)
+def gen_labels(input_files, fmt="{{i}}"):
+    inds = [{'i': i} for i in range(len(input_files))]
+    tpl = Template(fmt)
+    return map(tpl.render, inds)
 
-def montage_single(output_file, input_files, labels=True, label_format="{}"):
+def montage_single(output_file, input_files, labels=True, label_template="{{i}}"):
     if labels:
-        labels = gen_labels(input_files, label_format)
+        labels = gen_labels(input_files, label_template)
 
     print("montage {}".format(output_file))
 
     montage(output_file, input_files, labels)
 
-def montage_dirs(output_dir, input_dirs, labels=True, label_format="{}", glob_pattern='*.png'):
+def montage_dirs(output_dir, input_dirs, labels=True, label_template="{{i}}", glob_pattern='*.png'):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     assert os.path.isdir(output_dir)
@@ -38,7 +40,7 @@ def montage_dirs(output_dir, input_dirs, labels=True, label_format="{}", glob_pa
         img = os.path.basename(img)
         input_files = [os.path.join(d, img) for d in input_dirs]
         output_file = os.path.join(output_dir, img)
-        montage_single(output_file, input_files, labels, label_format)
+        montage_single(output_file, input_files, labels, label_template)
 
 def make_video(output_file, input_dir, img_pattern='m%06d.png'):
     cmd = ['ffmpeg',
@@ -55,14 +57,14 @@ def main(args):
     input_dirs = list(map(os.path.isdir, args.files))
     if all(input_dirs):
         # Multiple output images
-        montage_dirs(args.output, args.files, args.labels, args.label_format)
+        montage_dirs(args.output, args.files, args.labels, args.label_template)
 
         if args.video:
             make_video(args.video, args.output)
 
     elif not any(input_dirs):
         # Single output image
-        montage_single(args.output, args.files, args.labels, args.label_format)
+        montage_single(args.output, args.files, args.labels, args.label_template)
     else:
         print("ERROR: Can't mix directories and files as input")
 
@@ -78,8 +80,8 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--video', help='make video')
     parser.add_argument('-l', '--labels', action='store_true', default=False,
             help='enable labels')
-    parser.add_argument('-f', '--label-format', default='{}',
-            help='label format string')
+    parser.add_argument('-f', '--label-template', default='{{i}}',
+            help='label template string')
 
     args = parser.parse_args()
 
